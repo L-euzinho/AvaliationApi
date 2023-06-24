0201e3085dd87f03d2adcf2cd3915a2239d57c83
Leo-Chaves <leonardo.2021107241@unicap.br>
2023-06-24 02:57:27 -0300

    Teste de inicialização(@Bean) Criação do banco de dados h2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,25 @@
 package org.example;
 
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        SpringApplication.run(Main.class,args);
+
+    }
+
+    @Bean
+    public CommandLineRunner initialization(){
+        return args -> {
+            System.out.println("Helo world, Running");
+
+        };
+
     }
 }
